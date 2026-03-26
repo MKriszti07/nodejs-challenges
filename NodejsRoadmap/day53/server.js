f816aebc86b9f9323dc94a72c912601c9419c122
Krisztina Makovinyi <krisztina.makovinyi07@gmail.com>
2026-03-26 19:05:59 +0100

    Day 53: Dockerizing Nodejs Apps

---
+++ NodejsRoadmap/day53/server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        ok: true,
+        message: "Hello from Dockerized Node.js app!",
+        time: new Date().toISOString(),
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
